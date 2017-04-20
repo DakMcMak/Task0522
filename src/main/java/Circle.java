dde86dc6bd34804974af576c455c9376a2202476
DakMcMak <esbolov.galymzhan@mail.ru>
2017-04-20 23:00:12 +0600

    Initial commit

---
+++ src/main/java/Circle.java
@@ -0,0 +1,35 @@
+public class Circle {
+    public double x;
+    public double y;
+    public double radius;
+
+    public Circle(double x, double y, double radius)
+    {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    public Circle(double x, double y)
+    {
+        this.y = y;
+        this.radius = 10;
+        this.x=x;
+    }
+    public Circle(double radius)
+    {
+        this.radius = radius;
+        this.x=10;
+        this.y=10;
+    }
+    public Circle()
+    {
+        ///this.radius = radius;
+        //this.x=10;
+        //this.y=10;
+        this(10,10,10);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
